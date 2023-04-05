cbef5baf9f575ac2ff449b00607674ab0d8e491e
themikail <hello@mikailgokce.com>
2023-04-05 16:54:34 +0200

    change solution

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 console.clear();
 
 const input = document.querySelector('[data-js="toUpperCase"]');
-input.addEventListener("input", function () {
-  this.value = this.value.toUpperCase();
+input.addEventListener("input", (event) => {
+  event.target.value = event.target.value.toUpperCase();
 });
